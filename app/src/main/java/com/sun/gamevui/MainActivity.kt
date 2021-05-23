3925caa129706780f2c9be9b3f948bc4e47da8a3
manhhung23399 <manhhung23399@gmail.com>
2021-05-23 23:29:08 +0700

    task36583 init bottomNavigation

--- app/src/main/java/com/sun/gamevui/MainActivity.kt
+++ app/src/main/java/com/sun/gamevui/MainActivity.kt
@@ -2,10 +2,21 @@ package com.sun.gamevui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.sun.gamevui.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragmentNavHost) as NavHostFragment
+        val navController = navHostFragment.navController
+        binding.bottomNavigation.setupWithNavController(navController)
     }
 }
