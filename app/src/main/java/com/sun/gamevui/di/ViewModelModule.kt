ff8f145433d6a07a6902a27b5b9a9ca088a18312
manhhung23399 <manhhung23399@gmail.com>
2021-05-26 11:58:27 +0700

    task36589 create ui homescreen

--- app/src/main/java/com/sun/gamevui/di/ViewModelModule.kt
+++ app/src/main/java/com/sun/gamevui/di/ViewModelModule.kt
@@ -1,5 +1,9 @@
 package com.sun.gamevui.di
 
+import com.sun.gamevui.ui.HomeViewModel
+import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
-val viewModelModule = module { }
+val viewModelModule = module {
+    viewModel { HomeViewModel() }
+}
